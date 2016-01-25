17b540de91c068ea3e702bdf0f030840d74ba9a8
fishmwei <fishmwei@qq.com>
2016-01-25 14:37:17 +0800

    Initial Commit

---
+++ MWMarqueeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MWMarqueeDemo
+//
+//  Created by huangmingwei on 16/1/25.
+//  Copyright © 2016年 huangmingwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
